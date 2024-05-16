30e2f2b696ef98cdbad36c72942e4269ca57dc1e
testgithubtiwari <rt936649@gmail.com>
2024-05-16 19:25:01 +0530

    deployed

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,9 @@ import { ENV_VAR } from "../utils/variable.env.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(ENV_VAR.MONGO_URI);
+    const connectionInstance = await mongoose.connect(
+      `mongodb+srv://dharamrajtiwari:${process.env.MONGODB_PASS}@cluster0.mxhckjt.mongodb.net/targafy?retryWrites=true&w=majority&appName=Cluster0`
+    );
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
