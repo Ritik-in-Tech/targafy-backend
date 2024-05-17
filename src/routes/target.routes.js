80c89da4a0d643d40f152b56338d44e4894fcea3
testgithubtiwari <rt936649@gmail.com>
2024-05-17 18:54:01 +0530

    target routes changes

--- src/routes/target.routes.js
+++ src/routes/target.routes.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 const router = Router();
-import { createTarget, getAllTargets, getTargetById, updateTarget, deleteTarget } from "../controllers/target.controller.js";
+import {
+  createTarget,
+  getAllTargets,
+  getTargetById,
+  updateTarget,
+  deleteTarget,
+} from "../controllers/target.controller.js";
 
+// router to create target
 router.route("/add/:id").post(createTarget);
 
+// router to get all targets for specific business
 router.route("/all/:id").get(getAllTargets);
 
+// router to get the target by id , update and delete the target
 router
   .route("/:bid/:tid")
   .get(getTargetById)
