6bf19a8bf497c42afded065ba293fe84de65a8de
testgithubtiwari <rt936649@gmail.com>
2024-05-26 10:59:02 +0530

    bug solved for update jwt token done

--- src/controllers/user/updateUserName.controller.js
+++ src/controllers/user/updateUserName.controller.js
@@ -1,40 +1,60 @@
+import jwt from "jsonwebtoken";
 import { Businessusers } from "../../models/businessUsers.model.js";
 import { User } from "../../models/user.model.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const updateUserName = asyncHandler(async (req, res) => {
   try {
-    const userId = req?.user?._id;
-    const newName = req?.params?.newName;
-    // Construct the query to find the user by _id and update their name
+    const userId = req.user._id;
+    const newName = req.params.newName;
+
+    // Update user's name in User collection
     const updatedUser = await User.findOneAndUpdate(
-      { _id: userId }, // Find user by _id
-      { $set: { name: newName } }, // Update user's name
-      { new: true } // Return the updated document
+      { _id: userId },
+      { $set: { name: newName } },
+      { new: true }
     );
 
-    // console.log(updatedUser);
-    if (updatedUser.matchedCount == 0) {
+    if (!updatedUser) {
       return res
         .status(404)
         .json(new ApiResponse(404, {}, "User does not exist!!"));
     }
 
+    // Update user's name in BusinessUsers collection
     await Businessusers.updateMany(
       { userId: userId },
-      {
-        $set: {
-          name: newName,
-        },
-      }
+      { $set: { name: newName } }
+    );
+
+    // Generate new JWT token with updated user details
+    const userDetails = {
+      _id: updatedUser._id,
+      contactNumber: updatedUser.contactNumber,
+      name: updatedUser.name,
+    };
+
+    const authToken = jwt.sign(
+      { userDetails },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
     );
 
     return res
       .status(200)
-      .json(new ApiResponse(200, updatedUser, "Name updated successfully!!"));
+      .json(
+        new ApiResponse(
+          200,
+          { updatedUser, authToken },
+          "Name updated successfully!!"
+        )
+      );
   } catch (error) {
-    // console.log(error);
+    console.error("Error:", error);
     return res
       .status(500)
       .json(new ApiResponse(500, {}, "Internal Server Error"));
