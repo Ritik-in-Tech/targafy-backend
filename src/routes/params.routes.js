c78236d704a8cff603cadb53dabac4a16178851e
testgithubtiwari <rt936649@gmail.com>
2024-05-28 17:15:54 +0530

    params route modified

--- src/routes/params.routes.js
+++ src/routes/params.routes.js
@@ -1,10 +1,25 @@
 import { Router } from "express";
 const router = Router();
-import { createParam, getAllParams, getParamById, updateParam, deleteParam } from "../controllers/params.controller.js";
+import {
+  createParam,
+  getAllParams,
+  getParamById,
+  updateParam,
+  deleteParam,
+  getAssignedParams,
+} from "../controllers/params.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+// middlewares which verifies is request from the authorized user
+router.use(verifyJWT);
 
-router.route("/add/:id").post(createParam);
+// router to add paramerters
+router.route("/add/:businessId").post(createParam);
 
-router.route("/all/:id").get(getAllParams);
+// router to get all parameters
+router.route("/all/:businessId").get(getAllParams);
+
+// router to get params and assigned users for that params in a particular business
+router.route("/get/assigned-parameter/:businessId").get(getAssignedParams);
 
 router
   .route("/:bid/:pid")
