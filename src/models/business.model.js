bab2138165c3c827b1a29d0468398c15a8edf5ec
testgithubtiwari <rt936649@gmail.com>
2024-05-28 17:15:01 +0530

    param schema added in business model

--- src/models/business.model.js
+++ src/models/business.model.js
@@ -7,21 +7,18 @@ const commonStringConstraints = {
   default: "",
 };
 
-// const paramSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   type: {
-//     type: String,
-//     required: true,
-//     enum: ["TypeA", "TypeB"], // Assuming these are the only valid types
-//   },
-//   stats: {
-//     type: String, // Adjust this according to the actual structure of stats
-//     // required: true,
-//   },
-// });
+const paramSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    paramId: {
+      type: Schema.Types.ObjectId,
+    },
+  },
+  { _id: false }
+);
 const businessSchema = new Schema({
   businessCode: {
     type: String,
@@ -42,22 +39,10 @@ const businessSchema = new Schema({
   industryType: commonStringConstraints,
   city: commonStringConstraints,
   country: commonStringConstraints,
-  // email: {
-  //   type: String,
-  //   required: true,
-  //   unique: true,
-  //   validate: {
-  //     validator: function (value) {
-  //       // Simple regex for email validation
-  //       return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
-  //     },
-  //     message: (props) => `${props.value} is not a valid email address!`,
-  //   },
-  // },
-  // params: {
-  //   type: [paramSchema],
-  //   default: [],
-  // },
+  params: {
+    type: [paramSchema],
+    default: [],
+  },
   targets: [
     {
       type: Schema.Types.ObjectId,
