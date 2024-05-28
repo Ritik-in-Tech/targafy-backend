e44a174e1d5c95423b88ce9966e10e85ff546fab
testgithubtiwari <rt936649@gmail.com>
2024-05-28 17:15:24 +0530

    params controller made

--- src/controllers/params.controller.js
+++ src/controllers/params.controller.js
@@ -2,49 +2,107 @@ import { Params } from "../models/params.model.js";
 import { Business } from "../models/business.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { Businessusers } from "../models/businessUsers.model.js";
 
 // Create a new param
 const createParam = asyncHandler(async (req, res) => {
   try {
-    const { name, type, stats } = req.body;
+    const { name, charts, duration, description, userIds } = req.body;
 
     // Validate required fields
-    if (!name || !type || !stats) {
+    if (!name || !charts || !duration || !description || !userIds) {
       return res
         .status(400)
         .json(new ApiResponse(400, {}, "Please provide all required fields"));
     }
 
-    // Validate type field
-    if (type !== "TypeA" && type !== "TypeB") {
+    // Validate duration field
+    const validDurations = ["1stTo31st", "upto30days", "30days"];
+    if (!validDurations.includes(duration)) {
       return res
         .status(400)
-        .json(new ApiResponse(400, {}, "Invalid param type"));
+        .json(new ApiResponse(400, {}, "Invalid duration value"));
     }
 
-    const businessId = req.params.id;
-    console.log(businessId);
-    const business = await Business.findOne({ _id: businessId });
+    const businessId = req.params.businessId;
+    const business = await Business.findById(businessId);
 
-    // Validate required fields
+    // Validate business existence
     if (!business) {
       return res
         .status(400)
         .json(new ApiResponse(400, {}, "Please provide a valid businessId"));
     }
 
-    // Create a new param
+    // Validate userIds
+    const validUserIds = [];
+    const usersAssigned = [];
+    for (const userId of userIds) {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res
+          .status(400)
+          .json(
+            new ApiResponse(400, {}, `User with ID ${userId} does not exist`)
+          );
+      }
+      const businessUser = await Businessusers.findOne({ userId, businessId });
+      if (!businessUser) {
+        return res
+          .status(400)
+          .json(
+            new ApiResponse(
+              400,
+              {},
+              `User with ID ${userId} is not associated with this business`
+            )
+          );
+      }
+      if (businessUser.role === "Admin") {
+        return res
+          .status(400)
+          .json(
+            new ApiResponse(
+              400,
+              {},
+              "Admin can't assign itself as a parameters user"
+            )
+          );
+      }
+      validUserIds.push(userId);
+      usersAssigned.push({ userId, name: user.name });
+    }
+
+    if (validUserIds.length === 0) {
+      return res
+        .status(400)
+        .json(
+          new ApiResponse(
+            400,
+            {},
+            "Not any selected user exists in this business"
+          )
+        );
+    }
+
+    // Create a new Params document
     const param = new Params({
       name,
-      type,
-      stats,
+      businessId: business._id,
+      charts,
+      duration,
+      description,
+      usersAssigned,
     });
 
-    console.log(business.params);
+    // Save the Params document to the database
+    await param.save();
 
-    business.params.push(param);
+    // Add the parameter name and id to the business.params array
+    business.params.push({ name, paramId: param._id });
 
-    // Save the param to the database
+    // Save the updated Business document
     await business.save();
 
     return res
@@ -61,7 +119,7 @@ const createParam = asyncHandler(async (req, res) => {
 // Get all params
 const getAllParams = asyncHandler(async (req, res) => {
   try {
-    const id = req.params.id;
+    const id = req.params.businessId;
     const business = await Business.findOne({ _id: id });
     if (!business) {
       return res
@@ -72,6 +130,44 @@ const getAllParams = asyncHandler(async (req, res) => {
     return res
       .status(200)
       .json(new ApiResponse(200, { params }, "Param fetched successfully"));
+  } catch (error) {
+    console.error("Error:", error);
+    return res
+      .status(500)
+      .json(new ApiResponse(500, {}, "Internal Server Error"));
+  }
+});
+
+// get params and the number of assigned users to specifc business
+const getAssignedParams = asyncHandler(async (req, res) => {
+  try {
+    const businessId = req.params.businessId;
+    const business = await Business.findById(businessId);
+
+    if (!business) {
+      return res
+        .status(404)
+        .json(new ApiResponse(404, {}, "Business not found"));
+    }
+
+    // Retrieve the Params documents associated with the business
+    const paramsDetails = await Params.find({ businessId: business._id });
+
+    // Construct the response
+    const response = paramsDetails.map((param) => ({
+      name: param.name,
+      assignedUsersCount: param.usersAssigned.length,
+    }));
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          response,
+          "Assigned parameters retrieved successfully"
+        )
+      );
   } catch (error) {
     console.error("Error:", error);
     return res
@@ -169,4 +265,11 @@ const deleteParam = asyncHandler(async (req, res) => {
   }
 });
 
-export { createParam, getAllParams, getParamById, updateParam, deleteParam };
+export {
+  createParam,
+  getAllParams,
+  getParamById,
+  updateParam,
+  deleteParam,
+  getAssignedParams,
+};
