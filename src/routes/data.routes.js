9071bc61998e7f0273ec44f5f9121cf3024265da
testgithubtiwari <rt936649@gmail.com>
2024-05-30 17:08:50 +0530

    getData route added

--- src/routes/data.routes.js
+++ src/routes/data.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { addData } from "../controllers/data.controller.js";
+import { addData, getParamData } from "../controllers/data.controller.js";
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/add-data/:businessId/:parameterName").post(addData);
 
+// router to get data
+router.route("/get-user-data/:businessId/:paramName").get(getParamData);
 export default router;
