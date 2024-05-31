9e2c6b5f21b9449d2e7336a22a529cbf151431c3
testgithubtiwari <rt936649@gmail.com>
2024-05-31 11:47:28 +0530

    target user routes added

--- src/routes/data.routes.js
+++ src/routes/data.routes.js
@@ -4,6 +4,7 @@ import {
   addData,
   getParamData,
   getPreviousData,
+  getTargetToAddData,
 } from "../controllers/data.controller.js";
 const router = Router();
 
@@ -16,4 +17,7 @@ router.route("/get-user-data/:businessId/:paramName").get(getParamData);
 
 // router to get previus data of user
 router.route("/get-previous-data/:businessId/:paramName").get(getPreviousData);
+
+// router to get the target names for the specifc user
+router.route("/get-target-users/:businessId").get(getTargetToAddData);
 export default router;
