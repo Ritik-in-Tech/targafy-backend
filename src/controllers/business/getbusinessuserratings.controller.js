b63d82913fff8ae19987a374f0cfed97516df1de
testgithubtiwari <rt936649@gmail.com>
2024-06-07 17:16:25 +0530

    get business controller done

---
+++ src/controllers/business/getbusinessuserratings.controller.js
@@ -0,0 +1,45 @@
+import { Business } from "../../models/business.model.js";
+import { Usersratings } from "../../models/rating.model.js";
+import { ApiResponse } from "../../utils/ApiResponse.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
+import mongoose from "mongoose";
+
+const getBusinessUserRatings = asyncHandler(async (req, res) => {
+  const businessId = req.params.businessId;
+  try {
+    if (!businessId) {
+      return res
+        .status(400)
+        .json(new ApiResponse(400, {}, "BusinessID is not provided"));
+    }
+    const business = await Business.findOne({
+      _id: new mongoose.Types.ObjectId(businessId),
+    });
+    if (!business) {
+      return res
+        .status(400)
+        .json(new ApiResponse(400, {}, "Business not found"));
+    }
+    let usersRatings = await Usersratings.find({
+      businessId: new mongoose.Types.ObjectId(businessId),
+    })
+      .populate("userId", "name")
+      .lean(); // Convert to plain JavaScript object
+
+    usersRatings = usersRatings.map((rating) => ({
+      ...rating,
+      userId: rating.userId._id,
+      name: rating.userId.name,
+    }));
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, { usersRatings }, "Ratings fetched successfully!")
+      );
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(new ApiResponse(500, "Internal Server Error"));
+  }
+});
+
+export { getBusinessUserRatings };
