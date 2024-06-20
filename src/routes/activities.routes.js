72d016576deefbd49f6797e0ffccb6cdd93f358b
testgithubtiwari <rt936649@gmail.com>
2024-06-20 16:55:51 +0530

    some change in activity routes

--- src/routes/activities.routes.js
+++ src/routes/activities.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { getAllActivityBusiness } from "../controllers/activities.controller.js";
+import {
+  getAllActivityBusiness,
+  getSubordinateUserActivity,
+} from "../controllers/activities.controller.js";
+// import { getAllsubOrdinatesBusinessUsers } from "../controllers/business/getbusinessusers.controller.js";
 const router = Router();
 
 // router to verify that jwt token is still valid
@@ -8,5 +12,9 @@ router.use(verifyJWT);
 
 // router to get activity
 router.route("/get-activity/:businessId").get(getAllActivityBusiness);
+
+router
+  .route("/get-all-subordinatesUserActivity/:businessId")
+  .get(getSubordinateUserActivity);
 
 export default router;
