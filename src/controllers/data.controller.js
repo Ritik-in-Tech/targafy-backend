3afe0478c905946b38bf93c32fde4834ec6991bc
testgithubtiwari <rt936649@gmail.com>
2024-06-22 19:52:02 +0530

    add test data done

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -244,6 +244,234 @@ const addData = asyncHandler(async (req, res) => {
   }
 });
 
+const addTestData = asyncHandler(async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { todaysdata, comment, date } = req.body;
+    const parameterName = req.params.parameterName;
+    const businessId = req.params.businessId;
+
+    if (!todaysdata || !comment || !date) {
+      await session.abortTransaction();
+      session.endSession();
+      return res
+        .status(400)
+        .json(
+          new ApiResponse(
+            400,
+            {},
+            "Please add today's data and comment and date in req.body"
+          )
+        );
+    }
+    if (!parameterName || !businessId) {
+      await session.abortTransaction();
+      session.endSession();
+      return res
+        .status(400)
+        .json(
+          new ApiResponse(
+            400,
+            {},
+            "Provide parameter name and business id in params"
+          )
+        );
+    }
+
+    const userId = req.user._id;
+    if (!userId) {
+      await session.abortTransaction();
+      session.endSession();
+      return res
+        .status(401)
+        .json(new ApiResponse(401, {}, "Token expired please log in again"));
+    }
+
+    const user = await User.findById(userId).session(session);
+    if (!user) {
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(404).json(new ApiResponse(404, {}, "User not found"));
+    }
+
+    const business = await Business.findById(businessId).session(session);
+    if (!business) {
+      await session.abortTransaction();
+      session.endSession();
+      return res
+        .status(404)
+        .json(
+          new ApiResponse(
+            404,
+            {},
+            "Business not found, please check businessId again"
+          )
+        );
+    }
+
+    // const businessusers = await Businessusers.findOne({
+    //   userId: userId,
+    //   businessId: businessId,
+    // });
+
+    // if (businessusers.role === "Admin") {
+    //   return res
+    //     .status(404)
+    //     .json(
+    //       new ApiResponse(
+    //         404,
+    //         {},
+    //         "Only user and MiniAdmin can upload the data"
+    //       )
+    //     );
+    // }
+
+    const paramDetails = await Params.findOne({
+      name: parameterName,
+      businessId,
+    }).session(session);
+    if (!paramDetails) {
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(404).json(new ApiResponse(404, {}, "Param not found"));
+    }
+
+    const target = await Target.findOne({
+      paramName: parameterName,
+      businessId,
+    }).session(session);
+    if (!target) {
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(404).json(new ApiResponse(404, {}, "Target not found"));
+    }
+
+    const userAssigned = target.usersAssigned.some((user) =>
+      user.userId.equals(userId)
+    );
+    if (!userAssigned) {
+      await session.abortTransaction();
+      session.endSession();
+      return res
+        .status(403)
+        .json(
+          new ApiResponse(403, {}, "User is not assigned to this parameter")
+        );
+    }
+
+    // const formattedDate = moment(date).format("YYYY-MM-DD HH:mm:ss");
+
+    let dataAdd = await DataAdd.findOne({
+      parameterName,
+      userId,
+      businessId,
+    }).session(session);
+
+    if (dataAdd) {
+      dataAdd.data.push({
+        todaysdata,
+        comment,
+        createdDate: date,
+      });
+
+      const activity = new Activites({
+        userId: userId,
+        businessId,
+        content: `${user.name} added the data for ${parameterName} in ${business.name}`,
+        activityCategory: "Data Add",
+      });
+
+      await activity.save({ session });
+    } else {
+      dataAdd = new DataAdd({
+        parameterName,
+        data: [{ todaysdata, comment, createdDate: date }],
+        userId,
+        businessId,
+        createdDate: date,
+      });
+    }
+
+    const activity = new Activites({
+      userId: userId,
+      businessId,
+      content: `${user.name} added the data for ${parameterName} in ${business.name}`,
+      activityCategory: "Data Add",
+    });
+
+    await activity.save({ session });
+
+    await dataAdd.save({ session });
+
+    const targetValue = parseFloat(target.targetValue);
+    const todaysDataValue = parseFloat(todaysdata);
+
+    // Find the data entry for the parameterName
+    let userDataEntry = user.data.find(
+      (entry) =>
+        entry.name === parameterName && entry.dataId.equals(dataAdd._id)
+    );
+
+    if (userDataEntry) {
+      // Check if adding the new data would exceed the threshold
+      if (userDataEntry.targetDone + todaysDataValue > targetValue) {
+        await session.abortTransaction();
+        session.endSession();
+        return res
+          .status(400)
+          .json(
+            new ApiResponse(
+              400,
+              {},
+              "Cumulative data exceeds the threshold value"
+            )
+          );
+      }
+      // Update the existing entry
+      userDataEntry.targetDone += todaysDataValue;
+    } else {
+      // Check if the new data exceeds the threshold
+      if (todaysDataValue > targetValue) {
+        await session.abortTransaction();
+        session.endSession();
+        return res
+          .status(400)
+          .json(
+            new ApiResponse(
+              400,
+              {},
+              "Cumulative data exceeds the threshold value"
+            )
+          );
+      }
+      // Create a new entry
+      user.data.push({
+        name: parameterName,
+        dataId: dataAdd._id,
+        targetDone: todaysDataValue,
+      });
+    }
+
+    await user.save({ session });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, { dataAdd }, "Data added successfully"));
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    console.error("Error:", error);
+    return res
+      .status(500)
+      .json(new ApiResponse(500, {}, "Internal Server Error"));
+  }
+});
+
 const getParamDataSpecificUser = asyncHandler(async (req, res) => {
   try {
     const businessId = req.params.businessId;
@@ -748,4 +976,5 @@ export {
   getTargetToAddData,
   getParamData,
   getDailyTargetValue,
+  addTestData,
 };
