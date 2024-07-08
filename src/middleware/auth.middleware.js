29b4d9ffa0af91ca87266ba783fe1702aa4e697f
testgithubtiwari <rt936649@gmail.com>
2024-07-08 12:37:39 +0530

    acces token secret admin added

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,6 +2,8 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { ENV_VAR } from "../utils/variable.env.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
@@ -40,4 +42,46 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { verifyJWT };
+const verifyJWTAdmin = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      return res
+        .status(401)
+        .json(new ApiResponse(401, {}, "Unauthorized request"));
+    }
+
+    const decodedToken = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET_ADMIN
+    );
+
+    console.log(process.env.ACCESS_TOKEN_SECRET_ADMIN);
+
+    if (
+      !decodedToken ||
+      !decodedToken?.userDetails ||
+      !decodedToken?.userDetails?._id ||
+      !decodedToken?.userDetails?.name ||
+      !decodedToken?.userDetails?.contactNumber
+    ) {
+      return res
+        .status(401)
+        .json(new ApiResponse(401, {}, "The token is invalid!"));
+    }
+
+    req.user = decodedToken?.userDetails;
+    req.userDetails = decodedToken?.userDetails;
+    next();
+  } catch (error) {
+    // console.log(error);
+    return res
+      .status(401)
+      .json(new ApiResponse(401, {}, "Invalid access token"));
+  }
+});
+
+export { verifyJWT, verifyJWTAdmin };
