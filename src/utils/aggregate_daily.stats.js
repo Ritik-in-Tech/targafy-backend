0cb835b95d897bb8298c8e2d24c780b05534212c
testgithubtiwari <rt936649@gmail.com>
2024-07-22 11:55:14 +0530

    daily stats changed little bit

--- src/utils/aggregate_daily.stats.js
+++ src/utils/aggregate_daily.stats.js
@@ -35,8 +35,6 @@ const aggregateDailyStats = async () => {
       return true;
     }
 
-    let allUpdated = true;
-
     for (const business of businesses) {
       const businessId = business._id;
 
@@ -45,162 +43,122 @@ const aggregateDailyStats = async () => {
         date: previousDayStart,
       });
 
-      if (!existingStats) {
-        allUpdated = false;
+      const registeredUsers = await Businessusers.countDocuments({
+        businessId: businessId,
+        registrationDate: { $gte: previousDayStart, $lt: previousDayEnd },
+      });
 
-        const registeredUsers = await Businessusers.countDocuments({
-          businessId: businessId,
-          registrationDate: { $gte: previousDayStart, $lt: previousDayEnd },
-        });
+      const activeUsers = await Businessusers.countDocuments({
+        businessId: businessId,
+        lastSeen: { $gte: previousDayStart, $lt: previousDayEnd },
+      });
 
-        const activeUsers = await Businessusers.countDocuments({
-          businessId: businessId,
-          lastSeen: { $gte: previousDayStart, $lt: previousDayEnd },
-        });
+      const feedbackGiven = await Usersratings.countDocuments({
+        businessId: businessId,
+        createdDate: { $gte: previousDayStart, $lt: previousDayEnd },
+      });
 
-        const feedbackGiven = await Usersratings.countDocuments({
-          businessId: businessId,
-          createdDate: { $gte: previousDayStart, $lt: previousDayEnd },
-        });
-
-        const dataAddCount = await DataAdd.aggregate([
-          {
-            $match: {
-              businessId: new mongoose.Types.ObjectId(businessId),
-            },
+      const dataAddCount = await DataAdd.aggregate([
+        {
+          $match: {
+            businessId: new mongoose.Types.ObjectId(businessId),
           },
-          {
-            $addFields: {
-              matchingDates: {
-                $filter: {
-                  input: "$data",
-                  as: "dataItem",
-                  cond: {
-                    $and: [
-                      { $gte: ["$$dataItem.createdDate", previousDayStart] },
-                      { $lt: ["$$dataItem.createdDate", previousDayEnd] },
-                    ],
-                  },
+        },
+        {
+          $addFields: {
+            matchingDates: {
+              $filter: {
+                input: "$data",
+                as: "dataItem",
+                cond: {
+                  $and: [
+                    { $gte: ["$$dataItem.createdDate", previousDayStart] },
+                    { $lt: ["$$dataItem.createdDate", previousDayEnd] },
+                  ],
                 },
               },
             },
           },
-          {
-            $match: {
-              "matchingDates.0": { $exists: true },
-            },
+        },
+        {
+          $match: {
+            "matchingDates.0": { $exists: true },
           },
-          {
-            $count: "count",
+        },
+        {
+          $count: "count",
+        },
+      ]);
+
+      const dataAdd = dataAddCount.length > 0 ? dataAddCount[0].count : 0;
+
+      const messagesSent = await NotificationModel.countDocuments({
+        businessId: businessId,
+        createdDate: { $gte: previousDayStart, $lt: previousDayEnd },
+      });
+
+      const sevenDaysAgo = getStartOfNDaysAgo(7);
+
+      const statsOfLast7Days = await DailyStats.aggregate([
+        {
+          $match: {
+            businessId,
+            date: { $gte: sevenDaysAgo, $lt: previousDayStart },
           },
-        ]);
-
-        const dataAdd = dataAddCount.length > 0 ? dataAddCount[0].count : 0;
-
-        const messagesSent = await NotificationModel.countDocuments({
-          businessId: businessId,
-          createdDate: { $gte: previousDayStart, $lt: previousDayEnd },
-        });
-
-        // const totalSessionResult = await User.aggregate([
-        //   {
-        //     $match: {
-        //       "businesses.businessId": new mongoose.Types.ObjectId(businessId),
-        //     },
-        //   },
-        //   {
-        //     $unwind: "$lastSeenHistory",
-        //   },
-        //   {
-        //     $unwind: "$lastSeenHistory.lastSeen",
-        //   },
-        //   {
-        //     $match: {
-        //       "lastSeenHistory.lastSeen": {
-        //         $gte: new Date(previousDayStart),
-        //         $lt: new Date(previousDayEnd),
-        //       },
-        //     },
-        //   },
-        //   {
-        //     $group: {
-        //       _id: null,
-        //       totalSessions: { $sum: 1 },
-        //     },
-        //   },
-        // ]);
-
-        // const session =
-        //   totalSessionResult.length > 0
-        //     ? totalSessionResult[0].totalSessions
-        //     : 0;
-
-        const sevenDaysAgo = getStartOfNDaysAgo(7);
-
-        const statsOfLast7Days = await DailyStats.aggregate([
-          {
-            $match: {
-              businessId,
-              date: { $gte: sevenDaysAgo, $lt: previousDayStart },
-            },
+        },
+        {
+          $group: {
+            _id: null,
+            total_RegisteredUsers: { $sum: "$registeredUsers" },
+            total_ActiveUsers: { $sum: "$activeUsers" },
+            total_FeedbackGiven: { $sum: "$feedbackGiven" },
+            total_MessagesSent: { $sum: "$messagesSent" },
+            total_DataAdd: { $sum: "$dataAdd" },
+            count: { $sum: 1 },
           },
-          {
-            $group: {
-              _id: null,
-              total_RegisteredUsers: { $sum: "$registeredUsers" },
-              total_ActiveUsers: { $sum: "$activeUsers" },
-              total_FeedbackGiven: { $sum: "$feedbackGiven" },
-              total_MessagesSent: { $sum: "$messagesSent" },
-              total_DataAdd: { $sum: "$dataAdd" },
-              // total_SessionCount: { $sum: "$sessionCount" },
-              count: { $sum: 1 },
-            },
-          },
-        ]);
+        },
+      ]);
 
-        const avgStats = statsOfLast7Days[0] || {
-          total_RegisteredUsers: 0,
-          total_ActiveUsers: 0,
-          total_FeedbackGiven: 0,
-          total_MessagesSent: 0,
-          total_DataAdd: 0,
-          // total_SessionCount: 0,
-          count: 1,
-        };
+      const avgStats = statsOfLast7Days[0] || {
+        total_RegisteredUsers: 0,
+        total_ActiveUsers: 0,
+        total_FeedbackGiven: 0,
+        total_MessagesSent: 0,
+        total_DataAdd: 0,
+        count: 1,
+      };
 
-        const avg_RegisteredUsers =
-          avgStats.total_RegisteredUsers / avgStats.count;
-        const avg_FeedbackGiven = avgStats.total_FeedbackGiven / avgStats.count;
-        const avg_MessagesSent = avgStats.total_MessagesSent / avgStats.count;
-        const avg_ActiveUsers = avgStats.total_ActiveUsers / avgStats.count;
-        const avg_DataAdd = avgStats.total_DataAdd / avgStats.count;
-        // const avg_SessionCount = avgStats.total_SessionCount / avgStats.count;
+      const avg_RegisteredUsers =
+        avgStats.total_RegisteredUsers / avgStats.count;
+      const avg_FeedbackGiven = avgStats.total_FeedbackGiven / avgStats.count;
+      const avg_MessagesSent = avgStats.total_MessagesSent / avgStats.count;
+      const avg_ActiveUsers = avgStats.total_ActiveUsers / avgStats.count;
+      const avg_DataAdd = avgStats.total_DataAdd / avgStats.count;
 
-        await DailyStats.create({
-          businessId,
-          date: previousDayStart,
-          registeredUsers: registeredUsers,
-          activeUsers: activeUsers,
-          feedbackGiven: feedbackGiven,
-          messagesSent: messagesSent,
-          dataAdd: dataAdd,
-          // sessionCount: session,
-          avg_RegisteredUsers: avg_RegisteredUsers,
-          avg_ActiveUsers: avg_ActiveUsers,
-          avg_FeedbackGiven: avg_FeedbackGiven,
-          avg_MessagesSent: avg_MessagesSent,
-          avg_DataAdd: avg_DataAdd,
-          // avg_SessionCount: avg_SessionCount,
-        });
+      const statsData = {
+        businessId,
+        date: previousDayStart,
+        registeredUsers,
+        activeUsers,
+        feedbackGiven,
+        messagesSent,
+        dataAdd,
+        avg_RegisteredUsers,
+        avg_ActiveUsers,
+        avg_FeedbackGiven,
+        avg_MessagesSent,
+        avg_DataAdd,
+      };
+
+      if (existingStats) {
+        await DailyStats.updateOne({ _id: existingStats._id }, statsData);
+      } else {
+        await DailyStats.create(statsData);
       }
-      allUpdated = true;
     }
-    if (allUpdated) {
-      console.log("All business IDs have been processed for the previous day.");
-    } else {
-      console.log("Processing incomplete for some business IDs.");
-    }
-    return allUpdated;
+
+    console.log("All business IDs have been processed for the previous day.");
+    return true;
   } catch (error) {
     console.error("Error aggregating daily statistics:", error);
     return false;
@@ -222,8 +180,6 @@ const aggregateTestDailyStats = async (targetDate) => {
       return true;
     }
 
-    let allUpdated = true;
-
     for (const business of businesses) {
       const businessId = business._id;
 
@@ -232,162 +188,122 @@ const aggregateTestDailyStats = async (targetDate) => {
         date: previousDayStart,
       });
 
-      if (!existingStats) {
-        allUpdated = false;
+      const registeredUsers = await Businessusers.countDocuments({
+        businessId: businessId,
+        registrationDate: { $gte: previousDayStart, $lt: previousDayEnd },
+      });
 
-        const registeredUsers = await Businessusers.countDocuments({
-          businessId: businessId,
-          registrationDate: { $gte: previousDayStart, $lt: previousDayEnd },
-        });
+      const activeUsers = await Businessusers.countDocuments({
+        businessId: businessId,
+        lastSeen: { $gte: previousDayStart, $lt: previousDayEnd },
+      });
 
-        const activeUsers = await Businessusers.countDocuments({
-          businessId: businessId,
-          lastSeen: { $gte: previousDayStart, $lt: previousDayEnd },
-        });
+      const feedbackGiven = await Usersratings.countDocuments({
+        businessId: businessId,
+        createdDate: { $gte: previousDayStart, $lt: previousDayEnd },
+      });
 
-        const feedbackGiven = await Usersratings.countDocuments({
-          businessId: businessId,
-          createdDate: { $gte: previousDayStart, $lt: previousDayEnd },
-        });
-
-        const dataAddCount = await DataAdd.aggregate([
-          {
-            $match: {
-              businessId: new mongoose.Types.ObjectId(businessId),
-            },
+      const dataAddCount = await DataAdd.aggregate([
+        {
+          $match: {
+            businessId: new mongoose.Types.ObjectId(businessId),
           },
-          {
-            $addFields: {
-              matchingDates: {
-                $filter: {
-                  input: "$data",
-                  as: "dataItem",
-                  cond: {
-                    $and: [
-                      { $gte: ["$$dataItem.createdDate", previousDayStart] },
-                      { $lt: ["$$dataItem.createdDate", previousDayEnd] },
-                    ],
-                  },
+        },
+        {
+          $addFields: {
+            matchingDates: {
+              $filter: {
+                input: "$data",
+                as: "dataItem",
+                cond: {
+                  $and: [
+                    { $gte: ["$$dataItem.createdDate", previousDayStart] },
+                    { $lt: ["$$dataItem.createdDate", previousDayEnd] },
+                  ],
                 },
               },
             },
           },
-          {
-            $match: {
-              "matchingDates.0": { $exists: true },
-            },
+        },
+        {
+          $match: {
+            "matchingDates.0": { $exists: true },
           },
-          {
-            $count: "count",
+        },
+        {
+          $count: "count",
+        },
+      ]);
+
+      const dataAdd = dataAddCount.length > 0 ? dataAddCount[0].count : 0;
+
+      const messagesSent = await NotificationModel.countDocuments({
+        businessId: businessId,
+        createdDate: { $gte: previousDayStart, $lt: previousDayEnd },
+      });
+
+      const sevenDaysAgo = getStartOfNDaysAgo(7);
+
+      const statsOfLast7Days = await DailyStats.aggregate([
+        {
+          $match: {
+            businessId,
+            date: { $gte: sevenDaysAgo, $lt: previousDayStart },
           },
-        ]);
-
-        const dataAdd = dataAddCount.length > 0 ? dataAddCount[0].count : 0;
-
-        const messagesSent = await NotificationModel.countDocuments({
-          businessId: businessId,
-          createdDate: { $gte: previousDayStart, $lt: previousDayEnd },
-        });
-
-        // const totalSessionResult = await User.aggregate([
-        //   {
-        //     $match: {
-        //       "businesses.businessId": new mongoose.Types.ObjectId(businessId),
-        //     },
-        //   },
-        //   {
-        //     $unwind: "$lastSeenHistory",
-        //   },
-        //   {
-        //     $unwind: "$lastSeenHistory.lastSeen",
-        //   },
-        //   {
-        //     $match: {
-        //       "lastSeenHistory.lastSeen": {
-        //         $gte: new Date(previousDayStart),
-        //         $lt: new Date(previousDayEnd),
-        //       },
-        //     },
-        //   },
-        //   {
-        //     $group: {
-        //       _id: null,
-        //       totalSessions: { $sum: 1 },
-        //     },
-        //   },
-        // ]);
-
-        // const session =
-        //   totalSessionResult.length > 0
-        //     ? totalSessionResult[0].totalSessions
-        //     : 0;
-
-        const sevenDaysAgo = getStartOfNDaysAgo(7);
-
-        const statsOfLast7Days = await DailyStats.aggregate([
-          {
-            $match: {
-              businessId,
-              date: { $gte: sevenDaysAgo, $lt: previousDayStart },
-            },
+        },
+        {
+          $group: {
+            _id: null,
+            total_RegisteredUsers: { $sum: "$registeredUsers" },
+            total_ActiveUsers: { $sum: "$activeUsers" },
+            total_FeedbackGiven: { $sum: "$feedbackGiven" },
+            total_MessagesSent: { $sum: "$messagesSent" },
+            total_DataAdd: { $sum: "$dataAdd" },
+            count: { $sum: 1 },
           },
-          {
-            $group: {
-              _id: null,
-              total_RegisteredUsers: { $sum: "$registeredUsers" },
-              total_ActiveUsers: { $sum: "$activeUsers" },
-              total_FeedbackGiven: { $sum: "$feedbackGiven" },
-              total_MessagesSent: { $sum: "$messagesSent" },
-              total_DataAdd: { $sum: "$dataAdd" },
-              // total_SessionCount: { $sum: "$sessionCount" },
-              count: { $sum: 1 },
-            },
-          },
-        ]);
+        },
+      ]);
 
-        const avgStats = statsOfLast7Days[0] || {
-          total_RegisteredUsers: 0,
-          total_ActiveUsers: 0,
-          total_FeedbackGiven: 0,
-          total_MessagesSent: 0,
-          total_DataAdd: 0,
-          // total_SessionCount: 0,
-          count: 1,
-        };
+      const avgStats = statsOfLast7Days[0] || {
+        total_RegisteredUsers: 0,
+        total_ActiveUsers: 0,
+        total_FeedbackGiven: 0,
+        total_MessagesSent: 0,
+        total_DataAdd: 0,
+        count: 1,
+      };
 
-        const avg_RegisteredUsers =
-          avgStats.total_RegisteredUsers / avgStats.count;
-        const avg_FeedbackGiven = avgStats.total_FeedbackGiven / avgStats.count;
-        const avg_MessagesSent = avgStats.total_MessagesSent / avgStats.count;
-        const avg_ActiveUsers = avgStats.total_ActiveUsers / avgStats.count;
-        const avg_DataAdd = avgStats.total_DataAdd / avgStats.count;
-        // const avg_SessionCount = avgStats.total_SessionCount / avgStats.count;
+      const avg_RegisteredUsers =
+        avgStats.total_RegisteredUsers / avgStats.count;
+      const avg_FeedbackGiven = avgStats.total_FeedbackGiven / avgStats.count;
+      const avg_MessagesSent = avgStats.total_MessagesSent / avgStats.count;
+      const avg_ActiveUsers = avgStats.total_ActiveUsers / avgStats.count;
+      const avg_DataAdd = avgStats.total_DataAdd / avgStats.count;
 
-        await DailyStats.create({
-          businessId,
-          date: previousDayStart,
-          registeredUsers: registeredUsers,
-          activeUsers: activeUsers,
-          feedbackGiven: feedbackGiven,
-          messagesSent: messagesSent,
-          dataAdd: dataAdd,
-          // sessionCount: session,
-          avg_RegisteredUsers: avg_RegisteredUsers,
-          avg_ActiveUsers: avg_ActiveUsers,
-          avg_FeedbackGiven: avg_FeedbackGiven,
-          avg_MessagesSent: avg_MessagesSent,
-          avg_DataAdd: avg_DataAdd,
-          // avg_SessionCount: avg_SessionCount,
-        });
+      const statsData = {
+        businessId,
+        date: previousDayStart,
+        registeredUsers,
+        activeUsers,
+        feedbackGiven,
+        messagesSent,
+        dataAdd,
+        avg_RegisteredUsers,
+        avg_ActiveUsers,
+        avg_FeedbackGiven,
+        avg_MessagesSent,
+        avg_DataAdd,
+      };
+
+      if (existingStats) {
+        await DailyStats.updateOne({ _id: existingStats._id }, statsData);
+      } else {
+        await DailyStats.create(statsData);
       }
-      allUpdated = true;
     }
-    if (allUpdated) {
-      console.log("All business IDs have been processed for the previous day.");
-    } else {
-      console.log("Processing incomplete for some business IDs.");
-    }
-    return allUpdated;
+
+    console.log("All business IDs have been processed for the previous day.");
+    return true;
   } catch (error) {
     console.error("Error aggregating daily statistics:", error);
     return false;
